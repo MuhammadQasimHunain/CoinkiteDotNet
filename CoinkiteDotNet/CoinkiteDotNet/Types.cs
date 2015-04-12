5ef6739870f33c8012c94983319de0d3102cef6d
MrDunne <wdunne1@pca.ac.uk>
2015-04-12 04:57:39 +0100

    JSON deserialization

--- CoinkiteDotNet/CoinkiteDotNet/Types.cs
+++ CoinkiteDotNet/CoinkiteDotNet/Types.cs
@@ -12,5 +12,25 @@ namespace CoinkiteDotNet
         public string Data { get; set; }
     }
 
+    public class ApiKey
+    {
+        public string CK_refnum { get; set; }
+        public string CK_type { get; set; }
+        public string api_key { get; set; }
+        public object funds_limit { get; set; }
+        public int max_request_rate { get; set; }
+        public string memo { get; set; }
+        public List<string> permissions { get; set; }
+        public object source_ip { get; set; }
+    }
 
+    public class MySelf
+    {
+        public ApiKey api_key { get; set; }
+        public string member_since { get; set; }
+        public string membership { get; set; }
+        public List<string> nyms { get; set; }
+        public Dictionary<string,string> supported_cct { get; set; }
+        public string username { get; set; }
+    }
 }
--- CoinkiteDotNet/TestClient/Program.cs
+++ CoinkiteDotNet/TestClient/Program.cs
@@ -12,8 +12,10 @@ namespace TestClient
         static void Main(string[] args)
         {
             ICoinKite test = new ICoinKiteService("K4b092859-55e83adc-de62977147d346f4", "S097472f9-a831bd74-d730c43d58b79652");
+            string output = test.self();
 
-            System.Console.WriteLine(test.self());
+            System.IO.File.WriteAllText(@"C:\Users\william\Desktop\output.txt", output);
+            System.Console.WriteLine(output);
             System.Console.ReadLine();
         }
     }
